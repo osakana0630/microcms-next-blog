8de673d1eaff6a1117db5be269f6a2542bf81c27
osakana0630 <osakana0630@gmail.com>
2021-09-04 03:23:29 +0900

    add lint rules

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable */
 import Head from 'next/head'
 import Image from 'next/image'
+/* eslint-enable */
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
